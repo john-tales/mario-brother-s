b0cadb4ddb7902f560507873bca0875d2ada4b5b
john-tales <john.tales@hotmail.com>
2024-05-04 17:43:51 -0300

    introduçao do java script

---
+++ script.js
@@ -0,0 +1,8 @@
+const form = document.querySelector(".formulario-fale-conosco")
+const mascara = document.querySelector(".mascaraform")
+function  cliqueiNoBotao(){
+    form.style.left= "30%"
+    form.style.transform= "translateX(50%)" 
+    mascara.style.visibility= "visible"
+}
+
