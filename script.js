54f0ca1eec948463e7dd7bc91ccb2d7f95793a54
john-tales <john.tales@hotmail.com>
2024-05-05 09:05:09 -0300

    retornar o padrao ao clicar na mascara

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const form = document.querySelector(".formulario-fale-conosco")
 const mascara = document.querySelector(".mascaraform")
+
 function  cliqueiNoBotao(){
     form.style.left= "30%"
     form.style.transform= "translateX(50%)" 
     mascara.style.visibility= "visible"
 }
 
+function  cliqueiNaMascara(){
+    form.style.left= "-300px"
+    form.style.transform= "translateX(0)" 
+    mascara.style.visibility= "hidden"
+}
